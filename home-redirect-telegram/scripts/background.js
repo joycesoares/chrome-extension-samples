53c2fa4c469fa36b560c287208c9dc3be8472777
joycesoares <joyyceesooares@gmail.com>
2023-07-08 14:15:48 -0300

    enable extension only on specified website

--- home-redirect-telegram/scripts/background.js
+++ home-redirect-telegram/scripts/background.js
@@ -2,7 +2,7 @@ chrome.action.disable();
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status == "complete") {
-    if (tab.url.indexOf("telegram") != -1) {
+    if (tab.url.indexOf("https://web.telegram.org/a/") != -1) {
       chrome.action.enable(tabId);
     } else {
       chrome.action.disable(tabId);
